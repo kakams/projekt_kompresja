d0207a9a6e966ccea2eeff2505fe48fbfaf79731
DIYa2014 <dzuby1991@gmail.com>
2015-01-15 01:45:31 +0100

    BS: kompresja parami, dzielenie sms

--- SMSKompresor/app/src/main/java/pl/karbar/smskompresor/Utils/Bytes.java
+++ SMSKompresor/app/src/main/java/pl/karbar/smskompresor/Utils/Bytes.java
@@ -1,23 +1,27 @@
 package pl.karbar.smskompresor.Utils;
 
+import java.util.ArrayList;
+
 /**
  * Created by Dzuby on 2015-01-14.
  */
 public class Bytes {
 
-    public byte[] bytes;
+    public ArrayList<byte[]> bytesArray = new ArrayList<byte[]>();
     public String messageInBytes;
 
     public int lenghtBytes;
     public int lenghtMessage;
 
-    public Bytes(byte[] bytes, String messageInBytes, int lenghtBytes, int lenghtMessage ){
-
-        this.bytes = bytes;
+    public Bytes( String messageInBytes, int lenghtBytes, int lenghtMessage ){
         this.messageInBytes = messageInBytes;
         this.lenghtBytes = lenghtBytes;
         this.lenghtMessage = lenghtMessage;
 
+    }
+
+    public void addbytes(byte[] bytes){
+        bytesArray.add(bytes);
     }
 
 }
